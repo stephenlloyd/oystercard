e1708a764d08c3c2a93dad6ac3177cab2463aa90
Stephen Lloyd <stephen.lloyd85@gmail.com>
2015-08-28 09:59:25 +0100

    duck typing

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,7 +2,6 @@ class Journey
   attr_reader :entry_station, :exit_station
 
   PENALTY_FARE = 6
-  DESCENDING = Proc.new {|item_a,item_b| item_b <=> item_a }
 
   def initialize(entry_station: nil)
     @entry_station = entry_station
@@ -19,10 +18,11 @@ class Journey
   end
 
   def complete?
-    exit_station.respond_to? :zone
+    exit_station.respond_to?(:zone)
   end
 
   private
+  DESCENDING = Proc.new {|item_a,item_b| item_b <=> item_a }
 
   def zones
     [entry_station, exit_station].map(&:zone).sort(&DESCENDING)
